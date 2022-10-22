e50345a412cfd36e811c71e579f4aac94c73198d
Rose Felisme <rnfelism@gmial.com>
2022-10-22 13:15:19 -0400

    new code

---
+++ 8kyu/booleanToString.js
@@ -0,0 +1,8 @@
+// Convert a Boolean to a String
+
+// Implement a function which convert the given boolean value into its string representation.
+
+function booleanToString(){
+    //convert boolean value into a string value 
+    return b ? 'true': 'false;'
+}
